341b5fcd346f57e0bc3cad63a11719e10fe8bb30
Xiang Li <646873166@qq.com>
2022-11-07 01:31:54 +0800

    update UI again

--- assets/js/tree-adjust.js
+++ assets/js/tree-adjust.js
@@ -1,16 +1,19 @@
-!function () {
+let changeTreeSeason = function () {
     let tree = document.getElementById("elegant-tree-img");
     const d = new Date();
     let month = d.getMonth() + 1;
-
+    const delta = (-tree.width / 2).toString() + "px";
     if (month >= 3 && month <= 5)
-        tree.style.marginTop = "-125px";
+        tree.style.marginTop = delta;
     else if (month >= 6 && month <= 8)
         ;
     else if (month >= 9 && month <= 11)
-        tree.style.marginLeft = "-125px";
+        tree.style.marginLeft = delta;
     else {
-        tree.style.marginTop = "-125px";
-        tree.style.marginLeft = "-125px";
+        tree.style.marginTop = delta;
+        tree.style.marginLeft = delta;
     }
-}();
+};
+
+changeTreeSeason();
+window.addEventListener("resize", changeTreeSeason);
